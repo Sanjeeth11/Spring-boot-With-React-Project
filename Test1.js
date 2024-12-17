69ddd70925ebdefd7c2b405bf72e120455a03923
Sanjeeth11 <pisupatysaisanjeethraj@gmail.com>
2024-12-17 19:39:16 +0530

    single btn

--- Test1.js
+++ Test1.js
@@ -1,63 +1,107 @@
-import { useState } from "react"
+import React, {useState} from "react";
+const array = [
+    ["React is simple"],
+    ["Javascript is Simple"],
+    ["Java is simple"],
+    ["SpringBoot is simple"],
+    ["Spring is simple"],
+    ["Advance Java is simple"],
 
-function Test1(){
+]
 
-    const [index, setIndex] = useState(0);
+const menuoptions = [
+    {
+        id: 0,
+        name: 'React'
+    },
+    {
+        id: 1,
+        name: 'JavaScript'
+    },
+    {
+        id: 2,
+        name: 'Java'
+    },
+    {
+        id: 3,
+        name: 'SpringBoot'
+    },
+    {
+        id: 4,
+        name: 'Spring'
+    },
+    {
+        id: 5,
+        name: 'AdvanceJava'
+    }
 
-    const  array =[
-        [   "React is simple"],
-        [   "Javascript is Simple"],
-        [   "Java is simple"],
-        [   "SpringBoot is simple"],
-        [   "Spring is simple"],
-        [   "Advance Java is simple"],
+]
 
-    ]
-
+const Tabs = ({ index, setIndex }) => {
     return (
         <div>
-             <div class="button-container">
-    <button 
-    className={index === 0 ?"active":""}
-    onClick={()=> setIndex(0)}>React</button>
+            <menu>
+                {
+                    menuoptions.map(
+                        (singleoption) =>
+                            <Menu
+                                singleoption={singleoption}
+                                index={index}
+                                setIndex={setIndex}
+                            />
 
-    <button 
-    className={index === 1 ?"active":""}
-    onClick={()=> setIndex(1)}>Javascript</button>
+                    )
+                }
+            </menu>
 
-<button 
-    className={index === 2 ?"active":""}
-    onClick={()=> setIndex(2)}>Java</button>
-    
-<button 
-    className={index === 3 ?"active":""}
-    onClick={()=> setIndex(3)}>SpringBoot</button>
-    
-    <button 
-    className={index === 4 ?"active":""}
-    onClick={()=> setIndex(4)}>Spring</button>
-    
-    <button 
-    className={index === 5 ?"active":""}
-    onClick={()=> setIndex(5)}>Advance Java</button>
-   
 
-       <ul class="unorderedlist">
-        {
-            array[index].map(
-                item => (
-                    <li>
-                        {item}
-                    </li>
+        </div>
+    );
+}
+
+
+const Menu = ({singleoption, index, setIndex}) => {
+    return (
+        <div>
+            <button
+                className={index === singleoption.id ? "active" : ""}
+                onClick={() => setIndex(singleoption.id)}>
+                {singleoption.name}
+            </button>
+
+        </div>
+
+    );
+}
+
+const Display = ({ index }) => {
+    return (
+        <ul>
+            {   array[index].map(
+                    item => (
+                        <li>
+                            {item}
+                        </li>
+                    )
                 )
-            )
-        }
-       </ul>
-       </div>
-    </div>
+            }
+
+        </ul>
+    );
+}
 
 
-);
+const Test1 = () => {
+   
+    const [index, setIndex] = useState(0);
+    return (
+        <div>
+            <Tabs index={index}
+                setIndex={setIndex} />
+            <Display index={index} />
+        </div>
+    );
 }
 
 export default Test1;
+
